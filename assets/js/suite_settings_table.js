f2f5c6852430451dcc84d2fa3a42aa2c965d6b71
chengaomin <445462004@qq.com>
2017-04-18 22:36:59 +0800

    新增 suite级别的settings页面

---
+++ assets/js/suite_settings_table.js
@@ -0,0 +1,71 @@
+var suite_settings_table_tableDiv = document.getElementById('suite_settings_table'),
+    suite_settings_table_settings,
+    suite_settings_table_hot;
+
+
+
+var UIsuite_settings_table = function () {
+
+    var colorFirstCol = function () {
+        for (var i = 0; i < 9; i++) {
+            var cell = suite_settings_table_hot.getCell(i,0);
+            cell.style.backgroundColor = '#e6e6e6';
+            cell.style.color='black';
+            suite_settings_table_hot.getCellMeta(i, 0).readOnly=true
+        }
+
+    }
+
+    var renderTable = function () {
+
+
+
+        function getData() {
+            return [
+                ['Documentation', '', '', '', ''],
+                ['Suite Setup', '', '', '', ''],
+                ['Suite Teardown', '', '', '', ''],
+                ['Test Setup', '', '', '', ''],
+                ['Test Teardown', '', '', '', ''],
+                ['Test Template', '', '', '', ''],
+                ['Test Timeout', '', '', '', ''],
+                ['Force Tags', '', '', '', ''],
+                ['Default Tags', '', '', '', ''],
+
+            ];
+        }
+
+
+
+        suite_settings_table_settings = {
+            data: getData(),
+            minRows: 9,
+            minCols: 20,
+            rowHeights: 50,
+            colWidths: 150,
+            minSpareCols: 1,
+            // rowHeaders: ['Car', 'Year', 'Chassis color', 'Bumper color']
+        };
+        suite_settings_table_hot = new Handsontable(suite_settings_table_tableDiv, suite_settings_table_settings);
+        // setTimeout('colorFirstCol()',1000);
+
+    }
+
+    return {
+        init: function () {
+            renderTable();
+            // setTimeout('colorFirstCol()',1500);
+        },
+        colorFirstCol:function () {
+            colorFirstCol()
+        },
+    };
+
+}();
+
+
+jQuery(document).ready(function () {
+    UIsuite_settings_table.init();
+
+    Handsontable.hooks.add('afterRender', UIsuite_settings_table.colorFirstCol, suite_settings_table_hot);
+});
