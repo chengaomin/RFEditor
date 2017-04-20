b8d6ad4af173a5b0ec67484adb860f26d3bc15eb
chengaomin <445462004@qq.com>
2017-04-20 22:02:19 +0800

    新增 suite variable的表格

---
+++ assets/js/suite_variable_table.js
@@ -0,0 +1,47 @@
+var suite_variable_table_tableDiv = document.getElementById('suite_variable_table'),
+    suite_variable_table_settings,
+    suite_variable_table_hot;
+
+
+
+var UIsuite_variable_table = function () {
+
+    var renderTable = function () {
+
+
+
+        function getData() {
+            return [
+                ['', '', ''],
+
+            ];
+        }
+
+
+
+        suite_variable_table_settings = {
+            data: getData(),
+            minRows: 6,
+            minCols: 3,
+            rowHeights: 30,
+            colWidths: 273,
+            minSpareRows: 1,
+            colHeaders: ['Variable', 'Value', 'Comment']
+        };
+        suite_variable_table_hot = new Handsontable(suite_variable_table_tableDiv, suite_variable_table_settings);
+
+    }
+
+    return {
+        init: function () {
+            renderTable();
+        },
+
+    };
+
+}();
+
+
+jQuery(document).ready(function () {
+    UIsuite_variable_table.init();
+});
