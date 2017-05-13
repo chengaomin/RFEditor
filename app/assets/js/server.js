50fadc2d476ea36c6e79c7b40b4cddbbbbea128c
chengaomin <445462004@qq.com>
2017-05-13 11:53:08 +0800

    优化 使用socket通信

---
+++ app/assets/js/server.js
@@ -0,0 +1,27 @@
+var net = nodeRequire('net');
+var chatServer = net.createServer(),
+    clientList = [];
+
+chatServer.on('connection', function (client) {
+    client.name = client.remoteAddress + ':' + client.remotePort
+    client.write('Hi ' + client.name + '!\n');
+
+    clientList.push(client)
+
+    client.on('data', function (data) {
+        // $("#running_log").append(data.toString());
+        $('#running_log').append(html_encode(decodeUnicode(data.toString())));
+        console.log(data.toString());
+        client.write('1111111!\n');
+    })
+
+    client.on('end', function () {
+        clientList.splice(clientList.indexOf(client), 1); // 删除数组中的制定元素。这是 JS 基本功哦~  
+    })
+
+    client.on('error', function (e) {
+        console.log(e);
+    });
+})
+
+chatServer.listen(9000); 
---
+++ app/plugin/rflistener/RFListener - 副本.py
@@ -0,0 +1,47 @@
+import os.path
+import tempfile
+import sys
+import socket
+
+reload(sys)
+sys.setdefaultencoding('utf-8') 
+
+
+
+class RFListener:
+    ROBOT_LISTENER_API_VERSION = 2
+    def __init__(self, *args):
+        self.outpath = os.path.join(tempfile.gettempdir(), args[0],'runing.txt')
+        # outpath = 'd:/ll.txt'args[0]
+        self.outfile = open(self.outpath, 'w')
+        # self.server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        # self.server.connect(( "127.0.0.1", 9000))
+        # self.server.send('111dasdaasdsd')
+
+
+    def start_suite(self, name, attrs):
+        pass
+
+    def start_test(self, name, attrs):
+        self.outfile = open(self.outpath, 'a')
+        self.outfile.writelines("Starting test: %s\n" % attrs['longname'])
+        self.outfile.flush()
+        self.outfile.close()
+
+    def log_message(self, message):    
+        self.outfile = open(self.outpath, 'a')
+        self.outfile.writelines("%s : %s : %s\n" % (message['timestamp'],message['level'],message['message']))
+        self.outfile.flush()
+        self.outfile.close()
+
+    def end_test(self, name, attrs):
+        self.outfile = open(self.outpath, 'a')
+        self.outfile.writelines("Ending test: %s\n\n" % attrs['longname'])
+        self.outfile.flush()
+        self.outfile.close()
+
+    def end_suite(self, name, attrs):
+        pass
+        
+    def close(self):
+        self.outfile.close()
