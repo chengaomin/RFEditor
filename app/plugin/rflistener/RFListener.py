49449296be724c628240f48b90d33126a2d003f8
chengaomin <445462004@qq.com>
2017-05-13 11:53:55 +0800

    优化 listener使用socket发消息

--- app/plugin/rflistener/RFListener.py
+++ app/plugin/rflistener/RFListener.py
@@ -1,40 +1,35 @@
 import os.path
 import tempfile
 import sys
+import socket
 
 reload(sys)
 sys.setdefaultencoding('utf-8') 
 
+
+
 class RFListener:
     ROBOT_LISTENER_API_VERSION = 2
     def __init__(self, *args):
-        self.outpath = os.path.join(tempfile.gettempdir(), args[0],'runing.txt')
-        # outpath = 'd:/ll.txt'args[0]
-        self.outfile = open(self.outpath, 'w')
-
+        self.server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        self.server.connect(( "127.0.0.1", 9000))
+        
     def start_suite(self, name, attrs):
         pass
 
     def start_test(self, name, attrs):
-        self.outfile = open(self.outpath, 'a')
-        self.outfile.writelines("Starting test: %s\n" % attrs['longname'])
-        self.outfile.flush()
-        self.outfile.close()
+        self.server.send("Starting test: %s\n" % attrs['longname'])
 
     def log_message(self, message):    
-        self.outfile = open(self.outpath, 'a')
-        self.outfile.writelines("%s : %s : %s\n" % (message['timestamp'],message['level'],message['message']))
-        self.outfile.flush()
-        self.outfile.close()
+        self.server.send("%s : %s : %s\n" % (message['timestamp'],message['level'],message['message']))
+
 
     def end_test(self, name, attrs):
-        self.outfile = open(self.outpath, 'a')
-        self.outfile.writelines("Ending test: %s\n\n" % attrs['longname'])
-        self.outfile.flush()
-        self.outfile.close()
+        self.server.send("Ending test: %s\n\n" % attrs['longname'])
+        # print attrs['status']
 
     def end_suite(self, name, attrs):
         pass
         
     def close(self):
-        self.outfile.close()
+        pass
